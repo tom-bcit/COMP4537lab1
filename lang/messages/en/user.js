c4e6916389f8d37387d45d2f5cb8d2df4d5250a2
tomhebcit <the18@my.bcit.ca>
2025-01-17 14:01:52 -0800

    changed title to match lab requirement

--- lang/messages/en/user.js
+++ lang/messages/en/user.js
@@ -1,4 +1,4 @@
-export const appTitle = "Note Taking Application";
+export const appTitle = "Lab 1: JSON, Object Constructor, localStorage -- Tom He";
 export const read = "Read Notes";
 export const write = "Write Notes";
 export const updated = "Updated at: ";
